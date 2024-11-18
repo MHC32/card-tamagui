42c937551b741a22165bfbb9ab6fb0a28ae6f104
MHC32 <hantzmichaelchery6@gmail.com>
2024-11-18 11:53:25 -0500

    setup cards

--- App.tsx
+++ App.tsx
@@ -1,18 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import {YStack, Text} from 'tamagui'
-import config from './tamagui.config'
-import {tokens} from './tamagui.config'
-import {useFonts} from 'expo-font'
+import React from 'react';
+import { TamaguiProvider, Button, Card, styled, YStack, Text } from 'tamagui';
+import  config  from './tamagui.config';
+import { tokens } from './tamagui.config';
+import { useFonts } from 'expo-font' 
+
+// Styled Button
+
+
+
+
+
 
 export default function App() {
-  const [loaded]= useFonts({
-    Urbanist: require('./assets/fonts/Urbanist-Light.ttf'),
-    UrbanistBold: require('./assets/fonts/Urbanist-Bold.ttf')
-  })
+ const [loaded] = useFonts({
+   Urbanist: require('./assets/fonts/Urbanist-Medium.ttf'),
+   UrbanistBold: require('./assets/fonts/Urbanist-Bold.ttf')
+ })
+
+ if (!loaded) {
+   return null
+ }
+
   return (
-    <YStack w='100%' h='100%' bg={tokens.color.white} jc='center' ai='center' >
-      <Text>Hello from App.tsx</Text>
-    </YStack>
+    <TamaguiProvider config={config}>
+      <YStack
+       w='100%'
+       h= '100%'
+       bg={tokens.color.green}
+       jc='center'
+       ai='center'
+       br={tokens.radius.medium}
+      >
+       <YStack
+         w='330'
+         h='430'
+         bg={tokens.color.black}
+         br={tokens.radius.large}
+         ai='center'
+       >
+         <YStack w='90%' h='55%' bg={tokens.color.white} marginTop={20} br={tokens.radius.large} >
+
+         </YStack>
+       </YStack>
+      </YStack>
+    </TamaguiProvider>
   );
 }
-
--- tamagui.config.ts
+++ tamagui.config.ts
@@ -1,100 +1,112 @@
-import {createFont, createTamagui, createTokens} from 'tamagui'
-
-
-
-const interFont = createFont({
-    family: 'Urbanist, Helvetica, Arial, Sans-serif',
-    size: {
-      1 : 18,
-      2 :  20,
-      3 :  22,
-      4 :  24,
-      true: 18
-    },
-
-    lineHeight: {
-        1: 22,
-        true: 22
-    },
-
-    weight: {
-        1: 400,
-        2: 600,
-        3: 900,
-        true: 400,
-    },
-})
+import {createTokens, createTamagui, createFont} from 'tamagui';
 
 const size = {
-    small: 16,
-    medium: 19,
-    large: 22,
-    true: 16,
-  }
-  
-  const radius = {
-    small: 4,
-    medium: 8,
-    large: 12,
-    true: 8,
-  };
-  
-  const zIndex = {
-    low: 1,
-    medium: 10,
-    high: 100,
-    true: 1,
-  };
+  small: 16,
+  medium: 19,
+  large: 22,
+  true: 16,
+}
 
-  export const tokens = createTokens({
-    size,
-    radius,
-    zIndex,
-    space: {small:8, medium:12, large:16, true: 12},
-    color: {
-      white: '#FFFFFF',
-      black: '#383838',
-      green: '#87DD3B',
-      gray: '#AEAEAE',
+const radius = {
+  small: 4,
+  medium: 8,
+  large: 12,
+  true: 8,
+};
 
-    },
-  })
+const zIndex = {
+  low: 1,
+  medium: 10,
+  high: 100,
+  true: 1,
+};
 
-  const config = createTamagui({
-    tokens,
-  
-    fonts:{
-      heading: interFont,
-      body: interFont,
-    },
-  
-     themes: {
-        light: {
-          bg:  tokens.color.white,
-          color: tokens.color.gray,
-        },
-        dark: {
-          bg: tokens.color.black,
-          color: tokens.color.gray,
-        },
-       
+const interFont = createFont ({
+  family: 'Helvetica, Arial, Sans-serif, Urbanist',
+  size: {
+      1: 12,
+      2: 14,
+      3: 15,
+      true: 12,
+  },
+
+  lineHeight:{
+      2: 22,
+      true: 22
+  },
+
+  weight: {
+      1: 300,
+      // 2 will be 300
+      3: 600,
+      true: 300,
+  },
+
+  letterSpacing:{
+      1: 0,
+      2: -1,
+      true: 0
+  },
+
+  face: {
+      300: {normal: 'InterLight', italic: 'InterItalic'},
+      600: {normal: 'InterBold'},
+      true: {normal: 'InterLight', italic: 'InterItalic'},
+  },
+})
+
+
+export const tokens = createTokens({
+  size,
+  radius,
+  zIndex,
+  space: {small:8, medium:12, large:16, true: 12},
+  color: {
+    primary: '#3498db',
+    secondary: '#2ecc71',
+    white: '#FFFFFF',
+    black: '#383838',
+    green: '#87DD3B',
+    gray: '#AEAEAE',
+  },
+})
+
+
+const config = createTamagui({
+  tokens,
+
+  fonts:{
+    heading: interFont,
+    body: interFont,
+  },
+
+   themes: {
+      light: {
+        bg: '#f2f2f2',
+        color: tokens.color.black,
       },
-    
-      shorthands: {
-        px: 'paddingHorizontal',
-        f: 'flex',
-        m: 'margin',
-        w: 'width',
-        ai: 'alignItems',
-        bg: 'backgroundColor',
-        br: 'borderRadius',
-        h: 'height',
-        jc: 'justifyContent',
-        p: 'padding'
-      } as const,
-  })
+      dark: {
+        bg: '#111',
+        color: tokens.color.white,
+      },
+    },
+  
+    shorthands: {
+      px: 'paddingHorizontal',
+      f: 'flex',
+      m: 'margin',
+      w: 'width',
+      ai: 'alignItems',
+      bg: 'backgroundColor',
+      br: 'borderRadius',
+      h: 'height',
+      jc: 'justifyContent',
+      p: 'padding',
+      mt: 'marginTop',
+    } as const,
+})
 
-  type AppConfig = typeof config
+type AppConfig = typeof config
 
 
   declare module 'tamagui'{
